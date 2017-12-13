34739d3502cac22dc4b786f1f78da9cdec39113d
majorz <zahari@resin.io>
2017-12-13 11:33:29 +0200

    Conditions for the start script

---
+++ scripts/start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# Choose a condition for running WiFi Connect according to your use case:
+
+# 1. Is there Internet connectivity?
+# nmcli -t g | grep full
+
+# 2. Is there a default gateway?
+# ip route | grep default
+
+# 3. Is there an active connection?
+# nmcli -t c show --active | grep :
+
+# 4. Is there an active WiFi connection?
+nmcli -t c show --active | grep 802-11-wireless
+
+if [ $? -eq 0 ]; then
+    printf 'Skipping WiFi Connect'
+else
+    printf 'Starting WiFi Connect'
+    ./resin-wifi-connect
+fi
+
+while true; do
+    sleep 100
+done
